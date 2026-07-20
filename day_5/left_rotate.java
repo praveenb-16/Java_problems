452417481e53c4c9548347a581894fa1356f47c3
Praveen <praveen78720@gmail.com>
2026-07-20 10:29:23 +0530

    Refactor left_rotate and right_rotate classes to use swapping technique for array rotation

--- day_5/left_rotate.java
+++ day_5/left_rotate.java
@@ -1,14 +1,31 @@
 package day_5;
+// using manual_method
+// public class left_rotate {
+//     public static void main(String [] args){
+//         int arr[] = {-2,-4, 3, 5, 7};
+//         int n = arr.length;
+//         int first = arr[0];
+//         for(int i=0; i<n-1 ; i++){
+//             arr[i] = arr[i+1];
+//         }
+//         arr[n-1] = first;
+//         for(int num : arr){
+//             System.out.print(num + " ");
+//         }
+//     }
+//}
 
+//Using Swaping_Technique
 public class left_rotate {
     public static void main(String [] args){
         int arr[] = {-2,-4, 3, 5, 7};
         int n = arr.length;
-        int first = arr[0];
+        int temp;
         for(int i=0; i<n-1 ; i++){
+            temp = arr[i];
             arr[i] = arr[i+1];
+            arr[i+1] = temp;
         }
-        arr[n-1] = first;
         for(int num : arr){
             System.out.print(num + " ");
         }
--- day_5/right_rotate.java
+++ day_5/right_rotate.java
@@ -1,14 +1,34 @@
 package day_5;
 
+// Using manual method
+
+// public class right_rotate {
+//     public static void main(String [] args){
+//         int arr[] = {-2,-4, 3, 5, 7};
+//         int n = arr.length;
+//         int last = arr[n-1];
+//         for(int i=n-1; i>=1 ; i--){
+//             arr[i] = arr[i-1];
+//         }
+//         arr[0] = last;
+//         for(int num : arr){
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
+
+//Using Swapping_technique
+
 public class right_rotate {
     public static void main(String [] args){
         int arr[] = {-2,-4, 3, 5, 7};
         int n = arr.length;
-        int last = arr[n-1];
+        int temp;
         for(int i=n-1; i>=1 ; i--){
+            temp = arr[i];
             arr[i] = arr[i-1];
+            arr[i-1] = temp;
         }
-        arr[0] = last;
         for(int num : arr){
             System.out.print(num + " ");
         }
