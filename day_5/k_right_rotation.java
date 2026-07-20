7310ff0cdf49416e1a6f6f3c1c65e33f9fa741d5
Praveen <praveen78720@gmail.com>
2026-07-20 16:03:42 +0530

    Add k_right_rotation class with implementation for array right rotation

---
+++ day_5/k_right_rotation.java
@@ -0,0 +1,20 @@
+package day_5;
+import java.util.*;
+public class k_right_rotation {
+    public static void main(String [] args){
+         
+        int arr[] = {-2,-4, 3, 5, 7};
+        int n = arr.length;
+        int k =2;
+        int temp;
+        for(int i=0; i<k ; i++){
+           for(int j =n-1; j>0;j--){ 
+            temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+           }
+        }
+        
+        System.out.print(Arrays.toString(arr));
+    }
+}
